4f3c6d15a5a4e537c96ad6a39e29d4a478b3a71f
Min <esspacetoM@gmail.com>
2023-07-10 18:06:39 +0900

    주식을 사고팔기 가장 좋은 시점

---
+++ 7장_배열/12_주식을_사고팔기_가장_좋은_시점/정답/저점과_현재_값과의_차이_계산.py
@@ -0,0 +1,13 @@
+import sys
+
+def maxProfit(prices):
+    profit = 0
+    min_price = sys.maxsize
+    
+    for price in prices:
+        min_price = min(min_price, price)
+        profit = max(profit, price - min_price)
+        
+    return profit
+
+print(maxProfit([7, 1, 5, 3, 6, 4]))
