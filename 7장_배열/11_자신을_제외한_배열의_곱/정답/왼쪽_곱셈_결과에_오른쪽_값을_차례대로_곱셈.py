5f3b77df895e1067cd76dd3eeb940cef0d9cad66
Min <esspacetoM@gmail.com>
2023-07-10 18:03:26 +0900

    왼쪽 곱샘 결과에 오른쪽 값을 차례대로 곱셈

---
+++ 7장_배열/11_자신을_제외한_배열의_곱/정답/왼쪽_곱셈_결과에_오른쪽_값을_차례대로_곱셈.py
@@ -0,0 +1,17 @@
+def productExceptSelf(nums):
+    out = []
+    p = 1
+    
+    for i in range(0, len(nums)):
+        out.append(p)
+        p = p * nums[i]
+        
+    p = 1
+    
+    for i in range(len(nums) - 1, -1, -1):
+        out[i] = out[i] * p
+        p = p * nums[i]
+        
+    return out
+
+print(productExceptSelf([1, 2, 3, 4]))
